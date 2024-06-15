47c214141e93689c6079eadece28ce7e40521f37
Osamu Aoki <osamu@debian.org>
2024-06-15 17:01:57 +0900

    lua/plugins/astrocore.lua: default=quiet and plain
    * no auto pair
    * no CMP
    * no diagonostics
    * no editorconfig (toggle keymap)
    * no better-escape.nvim (jk for term too)
    
    Signed-off-by: Osamu Aoki <osamu@debian.org>

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -13,9 +11,9 @@ return {
     -- Configure core features of AstroNvim
     features = {
       large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-      autopairs = true, -- enable autopairs at start
-      cmp = true, -- enable completion at start
-      diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
+      autopairs = false, -- disable autopairs at start
+      cmp = false, -- disable completion at start
+      diagnostics_mode = 0, -- disable diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
       highlighturl = true, -- highlight URLs at start
       notifications = true, -- enable notifications at start
     },
@@ -27,9 +25,9 @@ return {
     -- vim options can be configured here
     options = {
       opt = { -- vim.opt.<key>
-        relativenumber = true, -- sets vim.opt.relativenumber
-        number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
+        relativenumber = false, -- sets vim.opt.relativenumber
+        number = false, -- sets vim.opt.number
+        spell = true, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
       },
@@ -37,6 +35,7 @@ return {
         -- configure global vim variables (vim.g)
         -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
         -- This can be found in the `lua/lazy_setup.lua` file
+        editorconfig = false, -- sets vim.opt.editorconfig
       },
     },
     -- Mappings can be configured through AstroCore as well.
@@ -46,26 +45,36 @@ return {
       n = {
         -- second key is the lefthand side of the map
 
-        -- navigate buffer tabs
-        ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-        ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
-
-        -- mappings seen under group name "Buffer"
-        ["<Leader>bd"] = {
+        -- mappings for toggling editor config
+        ["<leader>ue"] = {
           function()
-            require("astroui.status.heirline").buffer_picker(
-              function(bufnr) require("astrocore.buffer").close(bufnr) end
-            )
+            -- vim.g.cmp_disable == nil is treated as false
+            local notify = require("astrocore").notify
+            vim.g.editorconfig = vim.g.editorconfig == false
+            if vim.g.editorconfig then
+              notify "Enabled editorconfig"
+            else
+              notify "Disabled editorconfig"
+            end
           end,
-          desc = "Close buffer from tabline",
+          desc = "Toggle editorconfig †",
         },
 
-        -- tables with just a `desc` key will be registered with which-key if it's installed
         -- this is useful for naming menus
         -- ["<Leader>b"] = { desc = "Buffers" },
 
         -- setting a mapping to false will disable it
         -- ["<C-S>"] = false,
+      },
+      i = {
+        -- mappings for jk to <ESC> equivalents
+        ["jk"] = { "<esc>", desc = "Exit insert-mode †" },
+      },
+      -- mappings for jk and <esc><esc> to <C-\\><C-n> equivalents
+      -- Single <esc> may be used by some terminal program
+      t = {
+        ["jk"] = { "<C-\\><C-n>", desc = "Exit term-mode †" },
+        ["<esc><esc>"] = { "<C-\\><C-n>", desc = "Exit term-mode †" },
       },
     },
   },
