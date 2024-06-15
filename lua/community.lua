bfb6be368ae13725cc1ab3fc4a4b9b3d2d5bf1fa
Osamu Aoki <osamu@debian.org>
2024-06-15 17:01:57 +0900

    lua/community.lua: use community packages
    * statusline with explicit MODE text
    * breadcrumbs for context
    * language support
    
    Signed-off-by: Osamu Aoki <osamu@debian.org>

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -7,6 +5,24 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  -- add bash language pack
+  { import = "astrocommunity.pack.bash" },
+  -- add cpp language pack
+  { import = "astrocommunity.pack.cpp" },
+  -- add lua language pack
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  -- add markdown language pack
+  { import = "astrocommunity.pack.markdown" },
+  -- add python language pack
+  { import = "astrocommunity.pack.python-ruff" },
+  -- add toml language pack
+  { import = "astrocommunity.pack.toml" },
+  -- add yaml language pack
+  { import = "astrocommunity.pack.yaml" },
+  -- add mode text to statusline
+  { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
+  -- change statusline design to nvchad (no abs line#:col#, incompatible with above)
+  -- { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
+  -- add winbar with code context (breadcrumbs.nvim)
+  { import = "astrocommunity.recipes.heirline-vscode-winbar" },
 }
